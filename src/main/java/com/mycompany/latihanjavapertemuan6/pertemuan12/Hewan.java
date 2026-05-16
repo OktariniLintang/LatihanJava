a4cb506623a5cdbf1aedd5da0b42c32ad1dffb18
Lintang <oktarinilintang@gmail.com>
2026-05-16 21:08:58 +0700

    Pertemuan12 : Polimorfisme

---
+++ src/main/java/com/mycompany/latihanjavapertemuan6/pertemuan12/Hewan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihanjavapertemuan6.pertemuan12;
+
+/**
+ *
+ * @author Lintang
+ */
+public class Hewan {
+    public void bersuara(){
+        System.out.println("Hewan membuat suara...");
+    }
+    
+}
---
+++ src/main/java/com/mycompany/latihanjavapertemuan6/pertemuan12/KebunBinatang.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihanjavapertemuan6.pertemuan12;
+
+/**
+ *
+ * @author Lintang
+ */
+public class KebunBinatang {
+    public static void main(String[]args){
+        Hewan[] KumpulanHewan = new Hewan[3];
+        
+        KumpulanHewan[0] =new Hewan();
+        KumpulanHewan[1] = new Kucing();
+        KumpulanHewan[2] = new Anjing();
+        
+        System.out.println("=== Sinfoni Kebun Binatang ===");
+        for (Hewan h : KumpulanHewan ){
+            h.bersuara();
+        }
+    }
+    
+}
